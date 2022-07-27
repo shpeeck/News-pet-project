6ac170b92d396e6f3ec7e9d620c213091363d1c1
shpeeck <jenya_vas@mail.ru>
2022-07-27 16:06:27 +0300

    added pagination

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,  Http404
 from django.http import HttpRequest, HttpResponse
+from django.core.paginator import Paginator
 
 # Create your views here.
 posts = [
@@ -7,11 +8,18 @@ posts = [
     {'id': 2, 'title': 'Erik Jones Has Day He Won’t Soon Forget As Denny Backup At Bristol','date': 'May 27, 2017', 'description': 'Separated they right at the coast a large ocean. A small river named Duden flows by their place and ...', 'author_id': 'Jacob', 'foto': 'news54.jpg', 'category': 'DIVING'}, 
     {'id': 3, 'title': 'A Taste Of What We Like This Week At CookA Like This Current Week','date': 'May 27, 2017', 'description': 'Separatede in the coast of the a ocean. A small river named Duden flows by their place and ...', 'author_id': 'Jacob', 'foto': 'news55.jpg', 'category': 'CYCLING'},
     {'id': 4, 'title': 'Football Meras On The Back SepatheyTG G6 Will Have Theytics.','date': 'May 27, 2017', 'description': 'Separated they right at the coast a large ocean. A small river named Duden flows by their place and ...', 'author_id': 'Jacob', 'foto': 'news56.jpg', 'category': 'FOOTBALL'}, 
+    {'id': 5, 'title': 'Football Meras On The Back SepatheyTG G6 Will Have Theytics.','date': 'May 27, 2017', 'description': 'Separated they right at the coast a large ocean. A small river named Duden flows by their place and ...', 'author_id': 'Jacob', 'foto': 'news53.jpg', 'category': 'FOOTBALL'},
+    {'id': 6, 'title': 'Erik Jones Has Day He Won’t Soon Forget As Denny Backup At Bristol','date': 'May 27, 2017', 'description': 'Separated they right at the coast a large ocean. A small river named Duden flows by their place and ...', 'author_id': 'Jacob', 'foto': 'news54.jpg', 'category': 'DIVING'}, 
+    {'id': 7, 'title': 'A Taste Of What We Like This Week At CookA Like This Current Week','date': 'May 27, 2017', 'description': 'Separatede in the coast of the a ocean. A small river named Duden flows by their place and ...', 'author_id': 'Jacob', 'foto': 'news55.jpg', 'category': 'CYCLING'},
+    {'id': 8, 'title': 'Football Meras On The Back SepatheyTG G6 Will Have Theytics.','date': 'May 27, 2017', 'description': 'Separated they right at the coast a large ocean. A small river named Duden flows by their place and ...', 'author_id': 'Jacob', 'foto': 'news56.jpg', 'category': 'FOOTBALL'},
     ]
 
 def home(request):
-    context = {'posts': posts}
-    return render(request, 'news/index.html', context=context)
+    contact_list = posts
+    paginator = Paginator(contact_list, 3)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    return render(request, 'news/index.html', {'posts': contacts})
 
 def foo(request):
     return render(request, 'news/index_test.html')
