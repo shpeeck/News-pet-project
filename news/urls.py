01f5e566ae92616b2ef6978dae499c59b119c683
shpeeck <jenya_vas@mail.ru>
2022-08-03 15:58:53 +0300

    works categories

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import home, post
+from .views import home, post, politic, busines, sport, fashion, travel
 
 urlpatterns = [
     path('', home,  name="home"),
-    path('post/<int:post_id>/', post, name="post")
+    path('post/<int:post_id>/', post, name="post"),
+    path('politic/', politic, name="politic"),
+    path('busines/', busines, name="busines"),
+    path('sport/', sport, name="sport"),
+    path('fashion/', fashion, name="fashion"),
+    path('travel/', travel, name="travel"),
 ]
--- news/views.py
+++ news/views.py
@@ -3,7 +3,18 @@ from django.http import HttpRequest, HttpResponse
 from django.core.paginator import Paginator
 from .models import Posts, Like, Comments
 
-# Create your views here.
+
+def base(request, category):
+    contact_list = Posts.objects.filter(category=category).order_by('-created')
+    if request.method == "GET" and 'search' in request.GET:
+        search = request.GET['search']
+        contact_list = Posts.objects.filter(title__icontains=search)
+
+    paginator = Paginator(contact_list, 3)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    return render(request, 'news/category.html', {'posts': contacts})
+
 def home(request):
     contact_list = Posts.objects.all().order_by('-created')
     if request.method == "GET" and 'search' in request.GET:
@@ -15,6 +26,20 @@ def home(request):
     contacts = paginator.get_page(page)
     return render(request, 'news/index.html', {'posts': contacts})
 
+def politic(request):
+    return base(request, 'политика')
+
+def busines(request):
+    return base(request, 'бизнес')
+
+def sport(request):
+    return base(request, 'спорт')
+
+def fashion(request):
+    return base(request, 'мода')
+
+def travel(request):
+    return base(request, 'путешествия')
 
 def post(request, post_id):
     try:
